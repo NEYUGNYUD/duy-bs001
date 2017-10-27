92c455a100d7047b76ebd99debafe02e391a2605
Nguyen Van Duy <duypeo96@gmail.com>
2017-10-27 21:57:19 +0700

    finish header and menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,29 @@
 /**
  * Created by duype on 10/27/2017.
  */
-node_modules
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function() {
+    return gulp.src('assets/scss/custom/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('assets/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+    gulp.watch("assets/scss/custom/*.scss", ['sass']);
+    gulp.watch("*.html", ['sass']);
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ""
+        }
+    });
+});
+
